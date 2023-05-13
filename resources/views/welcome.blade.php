dd245a1126fcb5adc454e69e1ed184553db21199
farshidelf <farshidelf@yahoo.com>
2023-05-13 20:18:36 +0330

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Home') }}
+        </h2>
+    </x-slot>
+
+    <div class="container mx-auto px-4">
+        <br>
+        <h2 class=" text-gray-400 mix-blend-difference">Pay Star Shop</h2>
+    </div>
+</x-app-layout>
